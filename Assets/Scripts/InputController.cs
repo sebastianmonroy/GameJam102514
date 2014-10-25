8a7fa73804f7e022a634824d87046139fcf0a7d8
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-10-25 19:00:59 -0400

    jumps increase/decrease ridespeed, broken

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using InControl;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public PlayerStateManager player;
+	public bool StartButton;
+	public Vector2 LeftStick;
+	public Vector2 RightStick;
+	public bool AButton;
+	public bool BButton;
+	public bool XButton;
+	public bool YButton;
+	public float LeftTrigger;
+	public float RightTrigger;
+	public bool LeftBumper;
+	public bool RightBumper;
+
+	public void Start() {
+		InputManager.Setup();
+	}
+
+	void Update() {
+		InputManager.Update();
+		if (HasPlayer()) {
+			// Update input values
+			StartButton = player.device.GetControl(InputControlType.Start).IsPressed;
+			LeftStick = new Vector2(player.device.LeftStickX, player.device.LeftStickY);
+			RightStick = new Vector2(player.device.RightStickX, player.device.RightStickY);
+			AButton = player.device.GetControl(InputControlType.Action1).IsPressed;
+			BButton = player.device.GetControl(InputControlType.Action2).IsPressed;
+			XButton = player.device.GetControl(InputControlType.Action3).IsPressed;
+			YButton = player.device.GetControl(InputControlType.Action4).IsPressed;
+			LeftTrigger = player.device.GetControl(InputControlType.LeftTrigger).Value;
+			RightTrigger = player.device.GetControl(InputControlType.RightTrigger).Value;
+			LeftBumper = player.device.GetControl(InputControlType.LeftBumper).IsPressed;
+			RightBumper = player.device.GetControl(InputControlType.RightBumper).IsPressed; 
+		}
+	}
+
+	public void SetPlayer(PlayerStateManager newPlayer) {
+		//Debug.Log("Number of Devices Available: " + InputManager.Devices.Count);
+		if (newPlayer != null && newPlayer.playerNum <= 4 && newPlayer.playerNum >= 0) {
+			Debug.Log("Player " + newPlayer.playerNum + " connected");
+			this.player = newPlayer;
+		} else {
+			this.player = null;
+			StartButton = false;
+			LeftStick = Vector2.zero;
+			RightStick = Vector2.zero;
+			AButton = false;
+			BButton = false;
+			XButton = false;
+			YButton = false;
+			LeftTrigger = 0f;
+			RightTrigger = 0f;
+			LeftBumper = false;
+			RightBumper = false;
+		}
+	}
+
+	public void UnsetPlayer() {
+		Debug.Log("Player " + player.playerNum + " disconnected");
+		SetPlayer(null);
+	}
+
+	public bool HasPlayer() {
+		return (player != null && (player.playerNum >= 0) && player.device != null);
+	}
+}
--- Assets/Scripts/State Managers/RiderStateManager.cs
+++ Assets/Scripts/State Managers/RiderStateManager.cs
@@ -6,8 +6,9 @@ public class RiderStateManager : PlayerStateManager
 	public SimpleState rideState, jumpState, deathState, finishedState;
 
 	public float rideSpeed, jumpSpeed;
+	public float defaultRideSpeed, defaultJumpSpeed;
 
-	private Vector3 jumpDirection, circleCenter, riderToCenter;
+	private Vector3 jumpDirection, circleCenter, riderToCenter, normal, tangent;
 
 	public override void Start()
 	{
@@ -20,23 +21,35 @@ public class RiderStateManager : PlayerStateManager
 		Setup();
 	}
 
+	public override void Setup () 
+	{
+		AssignPlayer();
+
+		rideSpeed = rideSpeed / 360f * (2f * Mathf.PI *  MainStateManager.instance.CIRCLE_RADIUS);
+		defaultRideSpeed = defaultRideSpeed / 360f * (2f * Mathf.PI *  MainStateManager.instance.CIRCLE_RADIUS);
+
+		jumpSpeed = jumpSpeed / 360f * (2f * Mathf.PI *  MainStateManager.instance.CIRCLE_RADIUS);
+		defaultJumpSpeed = defaultJumpSpeed / 360f * (2f * Mathf.PI *  MainStateManager.instance.CIRCLE_RADIUS);
+
+		stateMachine.SwitchStates(setupState);
+	}
+
 	public override void Update ()
 	{
 		circleCenter = MainStateManager.instance.CIRCLE_CENTER;
-		riderToCenter = circleCenter - this.transform.position;
+		riderToCenter = this.transform.position - circleCenter;
+		normal = -riderToCenter.normalized;
+		this.transform.rotation = Quaternion.LookRotation(-Mathf.Sign(rideSpeed) * Vector3.forward, normal);
+		tangent = this.transform.right;
 
+		rideSpeed = Mathf.Sign(rideSpeed) * Mathf.Clamp(Mathf.Abs(rideSpeed), defaultRideSpeed/2f, defaultRideSpeed * 5f);
+		jumpSpeed = Mathf.Clamp(Mathf.Abs(jumpSpeed), defaultJumpSpeed/2f, defaultJumpSpeed * 5f);
 		Execute();
 	}
 
 	public override void Execute ()
 	{
 		stateMachine.Execute();
-	}
-
-	public override void Setup () 
-	{
-		AssignPlayer();
-		stateMachine.SwitchStates(setupState);
 	}
 
 	#region SETUP
@@ -74,18 +87,18 @@ public class RiderStateManager : PlayerStateManager
 
 		void Ride ()
 		{
-			this.transform.RotateAround(Vector3.zero, Vector3.forward, -rideSpeed * Time.deltaTime);
+			this.transform.RotateAround(Vector3.zero, -Vector3.forward, rideSpeed);
 		}
 
 		void Aim ()
 		{
 			Vector3 aimVector = new Vector3(input.LeftStick.x, input.LeftStick.y, 0f);
-			if (aimVector.magnitude >= 0.3f && Vector3.Dot(aimVector.normalized, riderToCenter.normalized) > 0.4f)
+			if (aimVector.magnitude >= 0.3f && Vector3.Dot(aimVector.normalized, riderToCenter.normalized) < -0.4f)
 			{
+				jumpDirection = aimVector.normalized;
 				Debug.DrawLine(this.transform.position, this.transform.position +  aimVector * 5f, Color.red);
 				if (input.AButton) 
 				{
-					jumpDirection = aimVector.normalized;
 					stateMachine.SwitchStates(jumpState);
 				}
 			}
@@ -99,13 +112,20 @@ public class RiderStateManager : PlayerStateManager
 
 	#region JUMP
 	void JumpEnter ()
-	{
-
+	{	
+		Debug.Log(Vector3.Dot(jumpDirection, tangent));
+		float speedDelta = defaultJumpSpeed/2f * (Vector3.Dot(jumpDirection, tangent));
+		if (Mathf.Sign(GetJumpAngle()) == Mathf.Sign(rideSpeed)) {
+			jumpSpeed = rideSpeed + speedDelta;
+		} else {
+			jumpSpeed = rideSpeed - speedDelta;
+		}
+		rideSpeed += jumpSpeed;
 	}
 
 	void JumpUpdate () 
 	{
-		this.transform.position += jumpSpeed * Time.deltaTime * jumpDirection;
+		this.transform.position += jumpSpeed * jumpDirection;
 		CircleDetection();
 		JumpInput();
 	}
@@ -116,22 +136,32 @@ public class RiderStateManager : PlayerStateManager
 
 		void CircleDetection ()
 		{
-			float distanceFromCenter = riderToCenter.magnitude;
+			Vector3 centerToRider = circleCenter - this.transform.position;
+			float distanceFromCenter = centerToRider.magnitude;
 			float circleRadius = MainStateManager.instance.CIRCLE_RADIUS;
-			Debug.Log(distanceFromCenter);
+
 			if (distanceFromCenter >= circleRadius)
 			{
-				this.transform.position = circleCenter - riderToCenter.normalized * circleRadius;
+				this.transform.position = circleCenter - centerToRider.normalized * circleRadius;
 				stateMachine.SwitchStates(rideState);
 			}
 		}
 
 	void JumpExit ()
 	{
-		Debug.Log(GetJumpAngle());
+		// set correct direction for rider movement around circle
+		if (GetJumpAngle() > 0) 
+		{
+			rideSpeed = Mathf.Abs(rideSpeed);
+		} 
+		else 
+		{
+			rideSpeed = -1f * Mathf.Abs(rideSpeed);
+		}
 	}
-		public float GetJumpAngle() {
-			Vector3 a = riderToCenter.normalized;
+		float GetJumpAngle() 
+		{
+			Vector3 a = riderToCenter;
 			Vector3 b = jumpDirection;
 			Vector3 n = Vector3.forward;
 			float sign = (Vector3.Dot(Vector3.Cross(a,b), n) > 0) ? -1 : 1;
